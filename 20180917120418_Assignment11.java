4b56e4bd31268363ace123c939e9511c41490b8e
amaandeep kaur <39096322+amanadeept@users.noreply.github.com>
2018-10-22 17:04:32 -0400

    Add files via upload

---
+++ 20180917120418_Assignment11.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1794404
+ */
+public class Assignment11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n;
+         Scanner console = new Scanner(System.in);
+               System.out.println("how many lines are");
+               n=console.nextInt();
+        int i,j;
+        for(i=0;i<n;i++){
+            
+            for(j=0;j<=i;j++) {
+                
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+        }}
+            
+       
+            
+    
+              
+
